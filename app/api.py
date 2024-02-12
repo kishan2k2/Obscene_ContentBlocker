4de06463d821127502a3fc263f75fa7240fde9ef
Kishan <payadikishan@gmail.com>
2024-02-13 01:29:12 +0530

    removed static check.

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-from app.CallFuctions.check import staticCheck, youtube, scrapable, age_restricted, caption_check, Safe_browsing, staticCheck2
+from app.CallFuctions.check import staticCheck, youtube, scrapable, age_restricted, caption_check, Safe_browsing
 from app.CallFuctions.scrape import youtube_caption, article_scrape
 from app.CallFuctions.preprocess import data_preprocessing
 from app.CallFuctions.ML import huggingface
